bb2ee8f9e025d681939bc4a197f85488cbf59920
Hazza <hasanmohammed2506@gmail.com>
2026-09-11 11:54:24 +0530

    Update firewall_zones.sh

--- firewall_zones.sh
+++ firewall_zones.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-# Student Name:
-# Register Number:
+# Student Name:HASAN MOHAMMED S
+# Register Number:1U24IT041
 
 # 1. Display all available firewall zones
-# TODO
+sudo firewall-cmd --get-zones
 
 # 2. Display current default zone
-# TODO
-
+sudo firewall-cmd --get-default-zone
 
 # 3. Display information about public zone
-# TODO
+sudo firewall-cmd --zone=public --list-all
 
 # 4. Set default zone to internal
-# TODO
-
+sudo firewall-cmd --set-default-zone=internal
 
 # 5. Verify the new default zone
-# TODO
-
-
-
-
-
+sudo firewall-cmd --get-default-zone
 
 exit 0
